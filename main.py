d5404fe0c5088804aedbd64219fdbba7ae383d3e
Vitalii <vetali5700gmail.com>
2025-08-27 23:22:16 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from city.routes import city
+from temperature.routes import temperature
 
 app = FastAPI()
 app.include_router(city)
+app.include_router(temperature)
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello. This is city temperature service!"}
