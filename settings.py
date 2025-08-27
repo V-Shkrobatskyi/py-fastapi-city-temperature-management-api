aa1bb93efbb77823c428bf8ee81edfc353d7f906
Vitalii <vetali5700gmail.com>
2025-08-27 23:22:26 +0300

    update settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,12 @@ from typing import Optional
 
 class Settings(BaseSettings):
     DATABASE_URL: Optional[str] = "sqlite+aiosqlite:///./database.db"
+    WEATHER_API_URL: Optional[str]
+    WEATHER_API_KEY: Optional[str]
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
 
 
 settings = Settings()
